94b9103fa09835a4483a3b440716e95cb18c209f
Ben Craig <bcraig78789@icloud.com>
2020-01-20 13:08:03 +0000

    For loop example

--- testing.py
+++ testing.py
@@ -2,8 +2,7 @@ print("Hiya")
 print("Ben")
 print("David")
 
-print("yeah")
-
-
-print "ye"
+number = 200
+for num in range(0, number, 4):
+    print("Every 4 numbers from 0 to 200 are {}".format(num))
 
